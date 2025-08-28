ed960107767ebe7a5c0b1323e0d5f81b0674a021
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-08-28 20:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	let index = s1.indexOf(s2);
+	let index = s1.toLowerCase().indexOf(s2.toLowerCase());
 	if(index != -1) return index;
 	return -1;
 	
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
